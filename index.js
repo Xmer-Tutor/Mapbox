d7fb586de4c2641fd102a0eef39c9e02e8c7ed5e
Wiley Hilton <wileyhilton@gmail.com>
2020-03-18 15:41:21 -0500

    fixed deploy script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const spawn = require('cross-spawn')
 const { NODE_ENV, port } = process.env
-const cmd = NODE_ENV === 'production' ? ['yarn', ['serve', '-l', port]] : ['yarn', ['start']];
+const cmd = NODE_ENV === 'production' ? ['yarn', ['serve', 'build', '-l', port]] : ['yarn', ['start']];
 const app = spawn(...cmd)
 
 app.stdout.on('data', data => {
